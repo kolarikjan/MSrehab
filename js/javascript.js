f6c89fb419023ca4e6db5dab720285674fee074f
Jan Kolařík <kolarikjan02@gmail.com>
2022-04-03 18:05:33 +0200

    minor fixes and improvements

--- js/javascript.js
+++ js/javascript.js
@@ -14,4 +14,63 @@ $(document).ready(function () {
         navText: ["<img src='img/banner-prev.png'>", "<img src='img/banner-next.png'>"]
     });
 
+    
+    // map script
+    if ($('#map').length > 0) {
+        
+        var root = am5.Root.new("map");
+
+        root.setThemes([
+            am5themes_Animated.new(root)
+        ]);
+
+        var chart = root.container.children.push(
+            am5map.MapChart.new(root, {
+                panX: false,
+                panY: false,
+                wheelY: false,
+            })
+        );
+
+        var polygonSeries = chart.series.push(
+            am5map.MapPolygonSeries.new(root, {
+                geoJSON: am5geodata_czechRepublicLow
+            })
+        );
+        
+        polygonSeries.mapPolygons.template.setAll({
+            fill: am5.color("#e6edf1"),
+            strokeWidth:  2,
+            stroke: am5.color("#ffffff"),
+            toggleKey: "active",
+            interactive: true,
+        });
+
+        polygonSeries.mapPolygons.template.states.create("active", {
+            fill: am5.color("#b9453d"),
+        });
+        
+        
+        polygonSeries.mapPolygons.template.states.create("hover", {
+            fill: am5.color("#b9453d"),
+        });
+
+        var previousPolygon;
+
+        polygonSeries.mapPolygons.template.on("active", function (active, target) {
+            if (previousPolygon && previousPolygon != target) {
+                previousPolygon.set("active", false);
+            }
+            previousPolygon = target;
+        });
+
+
+        // click on region event
+
+        polygonSeries.mapPolygons.template.events.on("click", function(ev) {
+            // name of region
+            console.log(ev.target.dataItem.dataContext.name);
+        });
+
+    }
 });
